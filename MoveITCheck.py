a766a9792ae1447c01ac178bcbfde966a5a0e032
Andy Gill <ZephrFish@users.noreply.github.com>
2023-06-02 00:37:51 +0100

    Added HTTP & HTTPS

--- MoveITCheck.py
+++ MoveITCheck.py
@@ -2,22 +2,23 @@ import requests
 import argparse
 
 def check_host(host, output_file=None):
-    try:
-        response = requests.get(f"http://{host.strip()}/human2.aspx")
-        if response.status_code == 404:
-            result = f"{host}: compromised"
-        elif response.status_code == 302:
-            result = f"{host}: exploit not present"
-        else:
-            result = f"{host}: returned unexpected status code {response.status_code}"
-    except Exception as e:
-        result = f"An error occurred while checking {host}: {str(e)}"
-    
-    print(result)
-    
-    if output_file:
-        with open(output_file, 'a') as f:
-            f.write(result + '\n')
+    for protocol in ['http', 'https']:
+        try:
+            response = requests.get(f"{protocol}://{host.strip()}/human2.aspx")
+            if response.status_code == 404:
+                result = f"{protocol.upper()} {host}: compromised"
+            elif response.status_code == 302:
+                result = f"{protocol.upper()} {host}: exploit not present"
+            else:
+                result = f"{protocol.upper()} {host}: returned unexpected status code {response.status_code}"
+        except Exception as e:
+            result = f"An error occurred while checking {protocol.upper()} {host}: {str(e)}"
+        
+        print(result)
+        
+        if output_file:
+            with open(output_file, 'a') as f:
+                f.write(result + '\n')
 
 def check_file(file_name, output_file=None):
     try:
